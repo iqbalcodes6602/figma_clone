9e25045185d61caebf26dd059d435a1f6c75d2f7
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2024-05-14 11:46:08 +0530

    chore: Update npm dependencies and tailwind configuration

---
+++ components/Live.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LiveCursors from './cursor/LiveCursors'
+import { useOthers } from '@/liveblocks.config'
+
+const Live = () => {
+    const others = useOthers();
+    return (
+        <div>
+            <LiveCursors others = {others} />
+        </div>
+    )
+}
+
+export default Live
---
+++ components/cursor/LiveCursors.tsx
@@ -0,0 +1,10 @@
+import { LiveCursorProps } from '@/types/type'
+import React from 'react'
+
+const LiveCursors = ({others}:LiveCursorProps) => {
+  return (
+    <div>LiveCursors</div>
+  )
+}
+
+export default LiveCursors
